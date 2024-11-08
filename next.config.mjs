d4d2490163d7d53757d05a6ad87cdecf200a3047
ds <zakaryabaouali255@gmail.com>
2024-11-08 09:36:49 +0100

    fd

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,9 @@
 const nextConfig = {
     images: {
       domains: ['images.unsplash.com', 'cdn-icons-png.flaticon.com' , 'www.upload.ee', 'firebasestorage.googleapis.com' , 'img.freepik.com'],
+
     },
+    
   };
   
   export default nextConfig;
