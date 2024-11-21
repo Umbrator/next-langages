58b4ae4ea70024558d140275a0c5925108723228
Ocean_Dev2 <zakaryabaouali255@gmail.com>
2024-11-21 10:34:47 +0100

    fdf

---
+++ next-sitemap.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    siteUrl: 'https://oceanconnecting.com', // Replace with your site URL
+    generateRobotsTxt: true,           // Automatically generate robots.txt
+    changefreq: 'daily',               // Change frequency (optional)
+    priority: 0.7,                     // Default priority for pages (optional)
+    sitemapSize: 5000,                 // Split sitemaps if too large (optional)
+    robotsTxtOptions: {
+      policies: [
+        { userAgent: '*', allow: '/' },
+        { userAgent: 'BadBot', disallow: '/' },
+      ],
+    },
+  };
+  
