496b2de3652a5d0ddb9f14b5d317ed5f09d49b46
Ocean_Dev2 <zakaryabaouali255@gmail.com>
2024-12-05 16:17:55 +0100

    fd

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -13,6 +13,8 @@ import Testimonials from './header/Testimonials';
 import Contact from './header/Contact';
 import OfflineChat from './header/OfflineChat';
 
+import { GoogleAnalytics } from '@next/third-parties/google'
+import Script from "next/script";
 // Dynamically load Footer with no SSR
 const Footer = dynamic(() => import('./header/Footer'), { ssr: false });
 
@@ -47,6 +49,25 @@ const Page = () => {
 
       {/* Offline Chat */}
       <OfflineChat />
+        <GoogleAnalytics gaId="G-4F7NDLC4B4" />
+        <Script
+            src={`https://www.googletagmanager.com/gtag/js?id=G-4F7NDLC4B4`}
+            strategy="afterInteractive"
+        />
+        <Script
+            id="google-analytics"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+                __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-4F7NDLC4B4', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+        />
     </div>
   );
 };
